bbc0eca38d76f1725e92474904a7bc7dab73ec6f
RonnyChiang <ronnychiang1164@gmail.com>
2021-11-01 11:43:00 +0800

    add mongoose

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const restaurants = require('./restaurant.json').results
 
+// connect mongoose
+const mongoose = require('mongoose') // 載入 mongoose
+mongoose.connect('mongodb://localhost/restaurant-list') // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 // template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')

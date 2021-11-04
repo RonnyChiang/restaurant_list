51d6a7fabac310b031836cd6e82ecf4d0ab5d91c
RonnyChiang <ronnychiang1164@gmail.com>
2021-11-04 14:24:06 +0800

    add edit function

--- app.js
+++ app.js
@@ -70,6 +70,48 @@ app.post("/restaurants", (req, res) => {
     .catch(err => console.log(err))
 })
 
+// edit page
+app.get('/restaurants/:restaurantId/edit', (req, res) => {
+  const id = req.params.restaurantId
+  return restaurants.findById(id)
+    .lean()
+    .then(restaurant => res.render("edit", { restaurant }))
+    .catch(err => console.log(err))
+})
+
+// edit info
+app.post("/restaurants/:restaurantId/edit", (req, res) => {
+  const id = req.params.restaurantId
+  const {
+    name, name_en, category, image, location, phone, google_map, rating, description
+  } = req.body
+  return restaurants.findById(id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.name_en = name_en
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = google_map
+      restaurant.rating = rating
+      restaurant.description = description
+
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(err => console.log(err))
+})
+
+app.get("/restaurants-fake/:restaurantId", (req, res) => {
+  const id = req.params.restaurantId;
+  return restaurants
+    .findById(id)
+    .lean()
+    .then(restaurant => res.json({ restaurant }))
+    .catch(err => console.log(err));
+});
+
 // start and listen on the Express server
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`)

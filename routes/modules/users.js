54cd59ee5d7c8ee87300db67b96fab4c99001749
ronnychiang <ronnychiang1164@gmail.com>
2021-12-14 17:37:22 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,7 +1,8 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
-const { route } = require('..')
+// const { route } = require('..')
 const router = express.Router()
+const User = require('../../models/user')
 
 // route
 router.get('/login', (req, res) => {
@@ -10,6 +11,29 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('User already exists.')
+        res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      } else {
+        return User.create({
+          name,
+          email,
+          password
+        })
+          .then(() => res.redirect('/'))
+          .catch(err => console.log(err))
+      }
+    })
 })
 
 module.exports = router

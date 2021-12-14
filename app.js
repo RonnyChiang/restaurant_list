8150160ea5a57ce4a5bd4df9e325c3845bfa8b26
ronnychiang <ronnychiang1164@gmail.com>
2021-12-14 17:55:24 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override') // 載入 method-override
+const session = require('express-session')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const routes = require('./routes')  // 引用路由器
 require('./config/mongoose') // 引用mongoose
@@ -12,9 +17,16 @@ const app = express()
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+
 app.use(express.static('public')) // static files
 app.use(express.urlencoded({ extended: true }))  //  body-parser
 app.use(methodOverride('_method')) //methodOverride
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
 
 // 將 request 導入路由器
 app.use(routes)

1cc176835d60fdf3daefb1ff2b70e41fc7be0928
ronnychiang <ronnychiang1164@gmail.com>
2021-12-15 17:21:49 +0800

    feat: add register error message and bcrypt

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,7 +4,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 const passport = require('passport')
-
+const bcrypt = require('bcryptjs')
 // route
 router.get('/login', (req, res) => {
   res.render('login')
@@ -20,25 +20,45 @@ router.get('/register', (req, res) => {
 })
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: '除了您的大名，其他欄位都必須填寫！' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   User.findOne({ email })
     .then(user => {
       if (user) {
+        errors.push({ message: 'Email已經註冊過了！' })
         console.log('User already exists.')
-        res.render('register', {
+        return res.render('register', {
+          errors,
           name,
           email,
           password,
           confirmPassword
         })
-      } else {
-        return User.create({
+      }
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+        .then(hash => User.create({
           name,
           email,
-          password
-        })
-          .then(() => res.redirect('/'))
-          .catch(err => console.log(err))
-      }
+          password: hash // 用雜湊值取代原本的使用者密碼
+        }))
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
     })
 })
 
